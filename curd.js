ff807a57e9bc84f96036d6c7a71e5dc5ef35952e
Himanshimalviya123 <malviyahimanshi14@gmail.com>
2025-02-27 22:09:39 +0530

    update

---
+++ curd.js
@@ -0,0 +1,27 @@
+let fetchData=async ()=>{
+    let url='http://localhost:3000/hotel'
+    let res=await fetch(url,{method:"GET"})
+     let data=await res.json()
+    // console.log(res);
+    console.log(data);
+    
+    
+
+let show=document.querySelector("#show")
+data.map((e)=>{
+    show.innerHTML+=
+    `
+    <tr>
+        
+        <td>${e.name}</td>
+        <td>${e.age}</td>
+        <td>${e.adhar}</td>
+        <td>${e.mobileno}</td>
+        <td>${e.date}</td>
+        <td>${e.destination}</td>
+        <td>${e.person}</td>
+        <td>${e.price}</td>
+        <td>${e.person*price}</td>
+    </tr>
+    `})
+}
